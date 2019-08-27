e09c5b96f65b908a14004f1455dbcb9fc48c10f9
lyukio <lyukio@hotmail.com>
2019-08-27 00:48:11 -0300

    creating a simple bot response when a user send a message

---
+++ src/config.js
@@ -0,0 +1,4 @@
+exports.config = {
+    token: 'NjE1NzM2NTg3MzU0ODk4NDQy.XWSXZw.PfafzqxSEPbVq6GRRPiPGNzjAJ4',
+    prefix: "?"
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const {config} = require('./config');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.author.bot || !message.content.startsWith(config.prefix)) return
+  message.channel.send(`Olá, ${message.author.username}. Infelizmente ainda sou um bot recém-nascido, mas prometo que no futuro terei vários comandos lindos para você poder usar. ٩(◕‿◕｡)۶`);
+});
+
+client.login(config.token);
